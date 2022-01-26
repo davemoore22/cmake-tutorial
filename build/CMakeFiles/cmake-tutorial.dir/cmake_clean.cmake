819fcca5ac18e34ba27c0abe086007d762e2f8ce
Dave M <davemoore22@gmail.com>
2022-01-26 17:11:58 +0000

    Initial Commit

---
+++ build/CMakeFiles/cmake-tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake-tutorial.dir/main.cpp.o"
+  "cmake-tutorial"
+  "cmake-tutorial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake-tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dave/Development/wizardry/cmake-tutorial
+# Build directory: /home/dave/Development/wizardry/cmake-tutorial/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
