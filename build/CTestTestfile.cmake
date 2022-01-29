e5f25a8396a7ef5fc5f1c1d0c0ea0971257968f6
Dave M <davemoore22@gmail.com>
2022-01-29 15:26:02 +0000

    Cleaned up Initial Commit

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dave/Development/wizardry/cmake-tutorial
-# Build directory: /home/dave/Development/wizardry/cmake-tutorial/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
